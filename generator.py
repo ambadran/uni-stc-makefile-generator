f74ffe9d74b64324e930c07cb13c75eaacc1c1f6
AbdulRahman Badran <ambadran717@hotmail.com>
2024-03-14 02:58:09 +0200

    started developing generator.py

---
+++ generator.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+
+# Mandatory Program input
+mcu_variant = "STC8H8K64U_SKDIP28"  #TODO: get them from terminal input
+mcu_freq_khz = 35000  #TODO: get them from terminal input
+
+# Optional Project input
+project_name = 'test'  #TODO: default to the name of the folder created in
+                        #TODO:
+
+
+
+
+
